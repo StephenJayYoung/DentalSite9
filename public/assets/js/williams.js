84966a993769b2290af2c600dbb16c90bbe7e1b4
Stephen Young <stephenjayyoung@gmail.com>
2016-03-03 10:20:06 -0800

    mods to location section

--- public/assets/js/williams.js
+++ public/assets/js/williams.js
@@ -115,12 +115,12 @@ jQuery(document).ready(function($){
     // google maps script
     $('#google-map').gMap({
 
-        address: '4221 85th Street, Lubbock, TX',
+        address: '1144-C India Hook Rd, Rock Hill, SC',
         maptype: 'ROADMAP',
         zoom: 16,
         markers: [
             {
-                address: "4221 85th Street, Lubbock, TX 79423",
+                address: "1144-C India Hook Rd, Rock Hill, SC 29732",
                 icon: {
                     image: "assets/images/icons/map-icon-red.png",
                     iconsize: [32, 39],
